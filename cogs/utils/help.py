275d11ff003308852dff0ae59d137a7436c13a7c
SirThane <jerade.hensley@gmail.com>
2018-04-23 20:29:48 -0400

    I left a fucking print in help

--- cogs/utils/help.py
+++ cogs/utils/help.py
@@ -341,7 +341,6 @@ class Help(formatter.HelpFormatter):
         traceback.print_tb(error.original.__traceback__, file=sys.stderr)
 
     def __unload(self):
-        print('called __unload')
         self.bot.formatter = formatter.HelpFormatter()
         self.bot.add_command(orig_help)
 
