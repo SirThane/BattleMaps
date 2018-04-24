0463605df310238e189fb480373e0cbbc468ff7a
SirThane <jerade.hensley@gmail.com>
2018-04-23 21:58:39 -0400

    Added InvalidMapException for when a map can't be detected

--- cogs/utils/errors.py
+++ cogs/utils/errors.py
@@ -5,6 +5,18 @@ from discord.ext import commands
 
 
 class AWBWDimensionsException(commands.CommandError):
-    """Exception"""
+    """Exception raised when an AWBW text map with
+    an inconsistent amount of columns in its rows.
+    Catch the AssertionError raised by the converter
+    and raise this Exception"""
+
+    def __init__(self, message=None, *args):
+        super().__init__(message, *args)
+
+
+class InvalidMapException(commands.CommandError):
+    """Exception raised when `check_map()` fails
+    to return an `AWMap` instance."""
+
     def __init__(self, message=None, *args):
         super().__init__(message, *args)
