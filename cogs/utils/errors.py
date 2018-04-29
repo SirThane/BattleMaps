7d5d9b48c97964743104dc726ada94d5ec516b2f
SirThane <jerade.hensley@gmail.com>
2018-04-29 19:13:39 -0400

    Added FileSaveFailureError

--- cogs/utils/errors.py
+++ cogs/utils/errors.py
@@ -1,28 +1,33 @@
 """Custom Exception definitions"""
 
 
-from discord.ext import commands
+from discord.ext.commands import CommandError
 
 
-class AWBWDimensionsError(commands.CommandError):
+class AWBWDimensionsError(CommandError):
     """Exception raised when an AWBW text map with
     an inconsistent amount of columns in its rows.
     Catch the AssertionError raised by the converter
     and raise this Exception"""
 
 
-class InvalidMapError(commands.CommandError):
+class InvalidMapError(CommandError):
     """Exception raised when `check_map()` fails
-    to return an `AWMap` instance."""
+    to return an `AWMap` instance"""
 
 
-class NoLoadedMapError(commands.CommandError):
+class NoLoadedMapError(CommandError):
     """Exception raised when a command requiring
     a loaded map is invoked by a user without a map
     loaded"""
 
 
-class UnimplementedError(commands.CommandError):
+class UnimplementedError(CommandError):
     """Exception raised when a command is called
     using a feature that has not yet been
     implemented"""
+
+
+class FileSaveFailureError(CommandError):
+    """Exception raised when a failure prevents an
+    attachment from being saved"""
